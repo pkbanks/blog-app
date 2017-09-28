598430d0c7f90239cdd547c3ff2044d50d0e9fa0
pkbanks <pkbanks@gmail.com>
2017-09-27 23:54:12 -0400

    updated how pages function and what functionality is made available based on whether the user is signed-in, and whether the user can edit or delete an article

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:edit, :update, :show]
-	
+
 	def new
 		@user = User.new
 	end
@@ -16,11 +16,11 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
+		set_user
 	end
 
 	def update
-		@user = User.find(params[:id])
+		set_user
 		if @user.update(user_params)
 			flash[:success] = "Your account was updated successfully."
 		else
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@user = User.find(params[:id])
+		set_user
 		@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 	end
 
@@ -42,5 +42,9 @@ class UsersController < ApplicationController
 
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
+	end
+
+	def set_user
+		@user = User.find(params[:id])
 	end
 end
