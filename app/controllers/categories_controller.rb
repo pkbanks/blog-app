9f2ad9ef79c9b007f7cdd4f231d7591061a6a942
pkbanks <pkbanks@gmail.com>
2017-09-30 11:15:17 -0400

    implementing integration tests for categories, new/create

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,9 +4,29 @@ class CategoriesController < ApplicationController
 	end
 
 	def new
+		@category = Category.new()
+	end
+
+	def create
+		@category = Category.new(category_params)
+		if @category.save
+			flash[:success] = "'#{@category.name}' category was created successfully"
+			redirect_to categories_path
+		else
+			render 'new'
+		end
 	end
 
 	def show
 	end
 
+	private
+	
+	def category_params
+		params.require(:category).permit(:name)
+	end
 end
+
+
+
+
---
+++ test/integration/create_categories_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class CreateCategoriesTest < ActionDispatch::IntegrationTest
+
+		test "get new category form and create category" do
+			get new_category_path			# get request
+			assert_template 'categories/new'		# go to new view, a form
+			assert_difference('Category.count', 1) do
+				# sends a post request to categories_path
+				post categories_path, params: {category: {name: 'sports'}}
+			end
+			# send to index view
+			assert_template 'categories/index'
+			assert_match "sports", response.body		# 'sports' should be in the body
+		end
+end
