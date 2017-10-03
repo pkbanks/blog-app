dc2135a710c5c2284629e2c16e56ea657470849a
pkbanks <pkbanks@gmail.com>
2017-10-03 08:05:05 -0400

    fixed integration tests for categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,18 +1,14 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
-
-		test "get new category form and create category" do
-
-			get new_category_path			# get request
-			assert_template 'categories/new'		# go to new view, a form
-			assert_difference('Category.count', 1) do
-				# sends a post request to categories_path
-				post categories_path, params: {category: {name: 'sports'}}
-			end
-			# send to index view
-			assert_template 'categories/index'
-			assert_match "sports", response.body		# 'sports' should be in the body
-		end
+  test "get new category form and create category" do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_difference 'Category.count', 1 do
+	    post categories_path, params: { category: {name: "sports"}}
+	    follow_redirect!
+    end
+    assert_template 'categories/index'
+    assert_match 'sports', response.body
+  end
 end
-
