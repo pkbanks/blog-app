6c862173a0b4e59075269f30716d6e7e9143330c
pkbanks <pkbanks@gmail.com>
2017-10-06 20:09:00 -0400

    fixed name of ListCategoriesTest, which had been CreateCategoriesTest

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CreateCategoriesTest < ActionDispatch::IntegrationTest
+class ListCategoriesTest < ActionDispatch::IntegrationTest
 
   def setup
     @category = Category.create(name: 'books')
@@ -10,7 +10,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   test "should show categories listing" do
     get categories_path
     assert_template 'categories/index'
-    assert_select "a[href=?]", category_path(@category), text: @category.name
-    assert_select "a[href=?]", category_path(@category2), text: @category2.name
+    assert_select "a[href=?]", category_path(@category), text: @category.name.capitalize
+    assert_select "a[href=?]", category_path(@category2), text: @category2.name.capitalize
   end
 end
