c260ea7bcd8d9ddc10d405af98cb55bdb43bbaa4
pkbanks <pkbanks@gmail.com>
2017-10-19 23:59:13 -0400

    added category show view with pagination

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,7 +21,8 @@ class CategoriesController < ApplicationController
 	end
 
 	def show
-		# @category = Category.find(category_params)
+		@category = Category.find(params[:id])
+		@category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
 	end
 
 	private
